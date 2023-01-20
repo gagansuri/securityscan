9bfb64d4acc0d145b5b4cbcd1646991f396d017b
GAGAN SURI <gagan.suri@gmail.com>
2023-01-20 12:17:23 -0600

    Update Authenticate.java

--- Authenticate.java
+++ Authenticate.java
@@ -3,6 +3,7 @@ public class Authenticate {
   private static String PART1 = "MIND YOUR OWN ";
   private static String PART2 = "BUSINESS";
   private static String PWD = PART1 + PART2;
+  private static String PASSWORD = "MIND YOUR OWN BUSINESS";
   
   
   
