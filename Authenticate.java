1f8c6573b47ea571aa883758c00b64d991737fa0
GAGAN SURI <gagan.suri@gmail.com>
2023-01-20 12:11:13 -0600

    Create Authenticate.java

---
+++ Authenticate.java
@@ -0,0 +1,10 @@
+public class Authenticate {
+
+  private static final String PWD = "MIND YOUR OWN BUSINESS";
+  
+  
+  public boolean validate () {
+    return PWD.equals("MIND YOUR OWN BUSINESS");
+  }
+
+}
